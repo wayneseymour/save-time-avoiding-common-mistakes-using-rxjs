dace537bdb653103ee7e9adf510d0e4d49513170
Tre' Seymour <wayne.seymour@elastic.co>
2022-08-12 11:18:30 +0100

    Compose the observables.

--- index.js
+++ index.js
@@ -3,21 +3,26 @@
 // 3. Compose
 
 const click$ = Rx.Observable.fromEvent(document, "click");
+
 const res$ = Rx.Observable.from(
-  fetch("https://api.quotable.io/random").then((x) => x.json())
+  fetch("https://api.quotable.io/random")
+    .then((x) => x.json())
+    .then(x => {
+      console.log(`\n### x: \n${JSON.stringify(x, null, 2)}`)
+      return x
+    })
 );
 
-const one$ = Rx.Observable.merge(click$, res$).map(
-  function makeThemBothOutput1() {
+const count$ = Rx.Observable.merge(click$, res$)
+  .map(function mapBothStreamsTo1() {
     return 1;
-  }
-);
-
-const count$ = one$.scan((acc, x) => acc + x, 0);
+  })
+  .scan((acc, x) => acc + x, 0);
 
 count$.subscribe((x) => {
   console.log(`\n### x: \n\t${x}`) || displayInPreview(x);
 });
+
 function displayInPreview(string) {
   var newDiv = document.createElement("div");
   var newContent = document.createTextNode(string);
